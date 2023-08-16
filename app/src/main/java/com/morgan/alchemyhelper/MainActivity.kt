2c2c56d2c000a20526983d881113c984f6284bfa
morgan <morgan.english@canterbury.ac.nz>
2023-08-16 21:10:56 +1200

    Basic ocr implementation, need to tidy up flow and would be nice to allow multiple images to be taken and processed at once for larger collections

--- app/src/main/java/com/morgan/alchemyhelper/MainActivity.kt
+++ app/src/main/java/com/morgan/alchemyhelper/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.morgan.alchemyhelper
 
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
@@ -18,6 +22,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Button
@@ -37,6 +42,8 @@ import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.navigation.NavController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -44,12 +51,24 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import androidx.room.Room
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+import com.morgan.LevenshteinDistance
 import com.morgan.alchemyhelper.persistence.AppDatabase
 import com.morgan.alchemyhelper.persistence.Effect
 import com.morgan.alchemyhelper.persistence.EffectWithIngredients
 import com.morgan.alchemyhelper.persistence.Ingredient
 import com.morgan.alchemyhelper.persistence.IngredientWithEffects
 import com.morgan.alchemyhelper.ui.theme.AlchemyHelperTheme
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.Objects
 
 
 class MainActivity : ComponentActivity() {
@@ -60,7 +79,7 @@ class MainActivity : ComponentActivity() {
         db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "alchemy-database")
             .allowMainThreadQueries().build()
         db.clearAllTables()
-        val csvId = R.raw.skyrim_alchemy;
+        val csvId = R.raw.skyrim_alchemy
         Log.d("MAIN", "Loading csv with id $csvId")
 
         CSVReader().readCSV(applicationContext.resources.openRawResource(csvId), db)
@@ -109,6 +128,9 @@ class MainActivity : ComponentActivity() {
                         val ingredientIds = backStackEntry.arguments?.getString("ingredients")
                         ingredientIds?.let { string: String -> PossiblePotions(ingredientIds = string) }
                     }
+                    composable("GetImage") {
+                        GetImage(navController)
+                    }
                 }
             }
         }
@@ -136,13 +158,7 @@ class MainActivity : ComponentActivity() {
             Button(onClick = { navController.navigate("FindPotions") }) {
                 Text(text = "Find possible potions from selection")
             }
-            Button(onClick = {
-                Toast.makeText(
-                    applicationContext,
-                    "Coming soon...",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }) {
+            Button(onClick = { navController.navigate("GetImage") }) {
                 Text(text = "Find possible potions from image")
             }
             Spacer(modifier = Modifier.weight(1f))
@@ -397,6 +413,127 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    /**
+     * Adapted from
+     * https://medium.com/@dheerubhadoria/capturing-images-from-camera-in-android-with-jetpack-compose-a-step-by-step-guide-64cd7f52e5de
+     * and
+     * https://developers.google.com/ml-kit/vision/text-recognition/v2/android
+     */
+    @OptIn(ExperimentalCoilApi::class)
+    @Composable
+    fun GetImage(navController: NavController) {
+        val context = applicationContext
+        val file = context.createImageFile()
+        val uri = FileProvider.getUriForFile(
+            Objects.requireNonNull(context),
+            "$packageName.provider", file
+        )
+        val capturedImageUri = remember {
+            mutableStateOf<Uri>(Uri.EMPTY)
+        }
+        val cameraLauncher =
+            rememberLauncherForActivityResult(ActivityResultContracts.TakePicture()) {
+                capturedImageUri.value = uri
+            }
+        val permissionLauncher = rememberLauncherForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) {
+            if (it) {
+                Toast.makeText(context, "Permission Granted", Toast.LENGTH_SHORT).show()
+                cameraLauncher.launch(uri)
+            } else {
+                Toast.makeText(context, "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            if (capturedImageUri.value.path?.isNotEmpty() == true) {
+                Image(
+                    modifier = Modifier
+                        .padding(16.dp, 8.dp).size(400.dp),
+                    painter = rememberImagePainter(capturedImageUri.value),
+                    contentDescription = null,
+                )
+            }
+            Button(onClick = {
+                val permissionCheckResult =
+                    ContextCompat.checkSelfPermission(context, android.Manifest.permission.CAMERA)
+                if (permissionCheckResult == PackageManager.PERMISSION_GRANTED) {
+                    cameraLauncher.launch(uri)
+                } else {
+                    // Request permissions
+                    permissionLauncher.launch(android.Manifest.permission.CAMERA)
+                }
+            }) {
+                Text(text = "Capture Image From Camera")
+            }
+            Button(onClick = {
+                val image: InputImage
+                try {
+                    val recognizer =
+                        TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+                    image = InputImage.fromFilePath(context, capturedImageUri.value)
+                    recognizer.process(image).addOnSuccessListener { text ->
+                        val distanceCalc = LevenshteinDistance()
+                        val ingredients: MutableList<Ingredient> = mutableListOf()
+                        for (block in text.textBlocks) {
+                            for (line in block.lines) {
+                                val l = line.text.replace("[^A-Za-z0-9 ']", "").replace(")", "")
+                                for (ingredient in db.IngredientDao().getAll()) {
+                                    // value below checks for some level of equality with what is found
+                                    // getting a good number is more of an art than science
+                                    if(distanceCalc.findSimilarity(l, ingredient.name) > 0.7) {
+                                        ingredients.add(ingredient)
+                                    }
+                                }
+                            }
+                        }
+                        if (ingredients.isEmpty()) {
+                            Toast.makeText(
+                                context, "Couldn't find any ingredients, give it another go", Toast.LENGTH_SHORT
+                            ).show()
+                        } else {
+                            navController.navigate(
+                                "PossiblePotions/${
+                                    ingredients.map { it.ingredientId }.joinToString(",")
+                                }"
+                            )
+                        }
+                        Log.d("OCR", text.text)
+                    }.addOnFailureListener {
+                        Toast.makeText(
+                            context, "Whoops something went wrong, give it another go", Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }) {
+                Text(text = "This looks good")
+            }
+        }
+
+
+    }
+
+
+    private fun Context.createImageFile(): File {
+        // Create an image file name
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.ENGLISH).format(Date())
+        val imageFileName = "JPEG_" + timeStamp + "_"
+        val image = File.createTempFile(
+            imageFileName, /* prefix */
+            ".jpg", /* suffix */
+            externalCacheDir      /* directory */
+        )
+        return image
     }
 }
 
